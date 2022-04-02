891a8a079301aaeb08090fefc1edbd77ffa0e631
Benson Thomas <bensonthomascr7@gmail.com>
2022-04-02 15:04:13 +0530

    Created a form which displays weather data based on zipCode and Country name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import { byCountry } from 'country-code-lookup';
 
 function App() {
+  const [zip, setZip] = useState('');
+  const [country,setCountry]=useState('');
+  const handler=async(e)=>
+  {
+    e.preventDefault();
+    const {iso2}=byCountry(country)
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},${iso2}&appid=c6b6521bbfa0ecfa8b508528f3f9823e`, {
+      method: "GET"
+    });
+    const data=await res.json();
+    console.log(data);
+    setZip('');
+    setCountry('');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <form onSubmit={handler}>
+        <lable>ZipCode</lable>
+        <input type={'text'} onChange={(e)=>setZip(e.target.value)} value={zip}/>
+        <label>Country</label>
+        <input type={'text'} onChange={(e)=>setCountry(e.target.value)} value={country}/>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
