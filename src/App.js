9f10b0cb3a2ccf2d75051ad8bbe73d601d3222a8
Benson Thomas <bensonthomascr7@gmail.com>
2022-04-19 13:52:26 +0530

    Added comments

--- src/App.js
+++ src/App.js
@@ -8,13 +8,17 @@ import { WeatherCard } from './components/WeatherCard/WeatherCard';
 
 function App() 
 {
+  // loading spinner jsx code
   const middleware=(
   <div className="spinner-border text-primary" role="status">
     <span className="visually-hidden">Loading...</span>
   </div>)
   const [getData, setGetData] = useState({});
+  // this state is triggered admist request and response of data
   const [isLoading, setIsLoading] = useState(false);
   const [coord,setCoord]=useState([]);
+  
+  // function to get weather data based on zip code and country entered by the user 
   const getByZip=async(iso2,zip)=>
   {
     setIsLoading(true);
@@ -39,32 +43,15 @@ function App()
     })
     setIsLoading(false)
   }
-  console.log(getData);
   
+  // Function to get weather data based on location entered by the user 
   const getByLocation=async(location,coordinates)=>
   {
     setIsLoading(true);
     setCoord(coordinates);
-    //$ Get the state code and country code using location name,state name and country name 
-    // let response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${location},${stateOfLocation},${country}&key=ecdad83f9a43466cb0d2ef5d24876161`);
-    // const {results}=await response.json();
-    // console.log(results);
-    // //$ Get the coordinates of the entered location 
-    // let geometry={};
-    // for (const result of results) 
-    // {
-    //   if(result.components.state===stateOfLocation && result.components.country===country)
-    //   {
-    //     geometry=result.geometry;
-    //     break
-    //   }   
-    // }
-    // console.log(geometry);
     //$ Get the current,daily and hourly weather data based on the coordinates found above 
     const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates[1]}&lon=${coordinates[0]}&units=metric&exclude=minutely,alerts&appid=c6b6521bbfa0ecfa8b508528f3f9823e`);
     const {current,daily,hourly}=await response.json();
-    // const data=await response.json();
-    // console.log(data);
     setGetData({
       location,
       current,
@@ -74,6 +61,7 @@ function App()
     setIsLoading(false);
   }
 
+  // Function to get weather data based on users current IP address 
   const getWeatherThroughIP=async()=>{
     setIsLoading(true);
     let response = await fetch(`https://api.ipdata.co/?api-key=4825cba494257a270b1a4e24386c124042b61f8e54720ac8a4ed04ec`);
--- src/components/Forms/searchByLocation.js
+++ src/components/Forms/searchByLocation.js
@@ -4,42 +4,40 @@ import classes from './location.module.css'
 
 const LocationForm = (props) => 
 {
+    // this state is used to get the location name when the use clicks one of the options
     const [location, setLocation] = useState('');
+    // this state is used to get the coordinates when the use clicks one of the options
     const [coordinates, setCoordinates] = useState([]);
+    // this state is used to display the autocomplete list
     const [options, setOptions] = useState([]);
-    // const [state, setState] = useState('');
-    // const [country, setCountry] = useState('');
+    
+    // form submit handler
     const handler=(e)=>
     {
+        // to prevent the page from reloading
         e.preventDefault();
+        // passing the data to the weather data getter function 
         props.onGet(location,coordinates);
         setLocation('');
-        // setState('');
-        // setCountry('');
     }
-    const changeHandler=async(e)=>
+    // autoComplete function
+    const autoComplete=async(e)=>
     {
         const res = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${e.target.value}.json?types=place%2Cpostcode%2Caddress%2Ccountry%2Clocality%2Cdistrict&autocomplete=true&access_token=pk.eyJ1IjoiYmVuc29uY3I3IiwiYSI6ImNsMGgxbHBwNjAyb3Qzb28yYno4ZzA1YWYifQ.JP1IaQwIySVQy5JZ8--aVg`);
         const data=await res.json();
         setOptions(data.features)
     }
+    
     return (
         <form onSubmit={handler} autoComplete={'off'}>
             <lable  htmlFor='location' className="form-label">Location</lable>
-            <input type={'text'} id="location" className={`form-control ${options.length===0 && "mb-3"}`} onChange={(e)=>{changeHandler(e); setLocation(e.target.value)}} autoComplete={`off`} value={location} />
+            <input type={'text'} id="location" className={`form-control ${options.length===0 && "mb-3"}`} onChange={(e)=>{autoComplete(e); setLocation(e.target.value)}} autoComplete={`off`} value={location} />
             
             {options.length!==0 && <ul className={`card ${classes['option-list']}`}>
                 {options.map((ele,i)=><div className='p-2' key={i} onClick={()=>{setLocation(ele.place_name);setCoordinates(ele.center);setOptions([])}}>{ele.place_name}</div>)}
             </ul>}
 
             <button className='btn btn-primary'>Submit</button>
-            
-            {/* <label htmlFor='state' className="form-label">State</label>
-            <input id="state" type={'text'} className="form-control mb-3" onChange={(e)=>setState(e.target.value)} value={state}/>
-  
-            <label htmlFor='country' className="form-label">Country</label>
-            <input id="country" type={'text'} className="form-control mb-3" onChange={(e)=>setCountry(e.target.value)} value={country}/> */}
-  
         </form>
     )
 }
