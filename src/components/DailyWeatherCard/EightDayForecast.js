44d3eefbe02f3b6dd3bc8798845edd72a446f6c2
Benson Thomas <bensonthomascr7@gmail.com>
2022-05-09 14:37:27 +0530

    Added timezone to display time according to country time

--- src/components/DailyWeatherCard/EightDayForecast.js
+++ src/components/DailyWeatherCard/EightDayForecast.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import DailyForecastAccordion from '../Accordion/DailyForecastAccordion'
 
-const EightDayForecast = ({dailyWeather,location,unit}) => {
+const EightDayForecast = ({dailyWeather,location,date,unit,timezone}) => {
+  date = new Date(date * 1000).toLocaleString('en-IN', {
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false,
+      timeZone: timezone,
+      timeZoneName: "short"
+    });
+
   return (
       <>
       <div className='bg-light'>
@@ -9,9 +17,10 @@ const EightDayForecast = ({dailyWeather,location,unit}) => {
             <div className='pb-5'>
               <span className='display-5 '>Daily Weather</span>
               <span className='lead fs-4'>- {location}</span>
+              <p>As of {date}</p>
             </div>
         <div className='accordion mx-5'>
-            {dailyWeather.map((element,i)=><DailyForecastAccordion key={i} index={i} data={element} unit={unit}/>)}
+            {dailyWeather.map((element,i)=><DailyForecastAccordion key={i} timezone={timezone} index={i} data={element} unit={unit}/>)}
         </div>
       </div>
       </div>
